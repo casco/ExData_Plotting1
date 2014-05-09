c16b154254076215973895e8bd098d1da6abbe3f
Alejandro Fernandez <alejandro.casco.fernandez@gmail.com>
2014-05-09 01:38:52 -0300

    Delete script.R

--- script.R
+++
@@ -1,5 +0,0 @@
-data <- read.table("household_power_consumption.txt", sep=";", header=T)
-data$Date <- strptime(data$Date, format="%d/%m/%Y")
-period.start <- strptime("01/02/2007", format="%d/%m/%Y")
-period.end <- strptime("02/02/2007", format="%d/%m/%Y")
-data.selected <- data[(period.start <= data$Date) &  (data$Date <= period.end),]
